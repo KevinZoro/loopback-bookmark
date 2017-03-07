2d8299f016bd093a65aed73420d4aa430e66db12
kevin <zhangtao4027050@gmail.com>
2017-03-07 12:11:48 +0800

    update

--- common/models/bookmark.js
+++ common/models/bookmark.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (Bookmark) {
-    Bookmark.list = (accessToken, limit, cb) => {
+    Bookmark.list = (accessToken, limit, skip,cb) => {
         if ((!(accessToken && accessToken.userId))) {
             cb(new Error('need login in'));
         }
@@ -13,19 +13,11 @@ module.exports = function (Bookmark) {
                     'type': 'public'
                 }, {
                     'userId': userId
-                }]
+                }],
             },
             order:"createTime desc,id desc",
-            limit
-            // "include": {
-            //     "relation": "users",
-            //     "scope":{
-            //         "fields": ["username"],
-            //         "where":{
-            //             "id":userId
-            //         }
-            //     }
-            // }
+            limit:limit,
+            skip:skip
         }, cb)
     }
 
@@ -43,11 +35,49 @@ module.exports = function (Bookmark) {
                         return ctx.req.accessToken;
                     }
                 },
-                { 'arg': 'limit', type: 'string' }
+                { 'arg': 'limit', type: 'string' },
+                { 'arg': 'skip', type: 'string'}
             ],
             returns: {
                 arg: 'list',
                 type: 'array'
+            }
+        }
+    )
+
+    Bookmark.listCount = (accessToken, cb) => {
+        if ((!(accessToken && accessToken.userId))) {
+            cb(new Error('need login in'));
+        }
+        let userId = accessToken.userId;
+        let app = Bookmark.app;
+        Bookmark.count({
+            'or': [{
+                'type': 'public'
+            }, {
+                'userId': userId
+            }],
+        }, cb)
+    }
+
+    Bookmark.remoteMethod(
+        'listCount', {
+            http: {
+                path: '/list/count',
+                verb: 'get'
+            },
+            accepts: [
+                {
+                    arg: 'accessToken',
+                    type: 'object',
+                    http: function (ctx) {
+                        return ctx.req.accessToken;
+                    }
+                }
+            ],
+            returns: {
+                arg: 'count',
+                type: 'number'
             }
         }
     )
